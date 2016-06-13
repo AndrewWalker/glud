314d1a3bff25fe2f4d48d28ea77a4338d62cda2d
Andrew Walker <walker.ab@gmail.com>
2016-06-13 22:31:47 +1000

    adding dropped functions back

--- glud/__init__.py
+++ glud/__init__.py
@@ -1,5 +1,6 @@
 from .display import dump
 from .parsing import *
+from .traversal import *
 from .predicates import *
 from .matchers import *
 from .version import __version__
---
+++ glud/traversal.py
@@ -0,0 +1,10 @@
+def walk(predicate, cursor):
+    """Recursively yield all descendant nodes in the tree
+    Find all (non-template) classes 
+    """
+    return (c for c in cursor.walk_preorder() if predicate(c))
+
+def iter_child_nodes(pred, cursor):
+    """Yield all direct child nodes of node
+    """
+    return (c for c in cursor.get_children() if predicate(c))
